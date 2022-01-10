c8bf60fe8b6cb96fc85e8c2816140c6ab7c624bb
01ddgodwin <59660523+01ddgodwin@users.noreply.github.com>
2022-01-09 21:07:58 -0700

    Add files via upload

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+static class GlazerApp
+{
+
+    public static void RunExample()
+    {
+        Console.WriteLine("Width: ");
+        var width = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Height: ");
+        var height = Convert.ToInt32(Console.ReadLine());
+
+        var woodLength = 2 * (width + height) * 3.25;
+        var glassArea = 2 * (width * height);
+
+        Console.WriteLine("The length of the wood is " +
+        woodLength + " feet");
+        Console.WriteLine("The area of the glass is " +
+        glassArea + " square metres");
+    }
+}
